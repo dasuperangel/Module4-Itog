6770f92abe9c2bd9b1c5bfdebf9fc0d3168c3417
dasuperangel <dasuperangel@gmail.com>
2022-04-26 18:10:27 +0300

    Добавьте файлы проекта.

---
+++ Module4 Itog/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Module4_Itog
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter your name: ");
+            string name = Console.ReadLine();
+            Console.Write("Enter your age: ");
+            byte age = (byte)int.Parse(Console.ReadLine());
+            Console.WriteLine("Your name is {0} and age is {1}", name, age);
+            Console.WriteLine("Enter your birthdate:");
+            string birthdate = Console.ReadLine();
+            Console.WriteLine("Your birthdate is {0}", birthdate);
+        }
+    }
+}
